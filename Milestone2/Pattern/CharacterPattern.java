b3f383cf3233825d46285959ef49baee5a0de18e
Aayush Kumar <53551688+ayush-kumar774@users.noreply.github.com>
2022-11-25 16:31:01 +0530

    Added Pattern 1 question's solution

---
+++ Milestone2/Pattern/CharacterPattern.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class CharacterPattern {
+    public static void pattern(int n)
+    {
+        int start = 'A' ;
+        for(int i = 1 ; i <= n ; i++)
+        {
+            start = start + i - 1;
+            for (int j = 1 ; j <= i ; j++) {
+                System.out.print((char)start);
+                start = start + 1 ;
+            }
+            System.out.println();
+            //start = start + 1;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pattern(n);
+       // int i = 1;
+        
+        // while ( i <= n ) {
+        //     int j = 1;
+        //     int  Start = 'A' + i - 1 ;
+        //     while (j <= i) {
+        //         System.out.print((char)(Start));
+        //         j++;
+        //        Start++;
+        //     }
+        //     System.out.println();
+        //     i++;
+        // }
+        sc.close();
+    }
+}
